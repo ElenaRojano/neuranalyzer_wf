761167af7b9055bcd29416505ea3772eae8f2991
elenarojano <elenarojano@uma.es>
2022-05-23 12:29:24 +0200

    Minor corrections: code not used was removed

--- launch.sh
+++ launch.sh
@@ -2,30 +2,20 @@
 
 #MODULES LOAD:
 hostname
+CODE_PATH=`pwd`
+
 . ~soft_bio_267/initializes/init_pets
 . ~soft_bio_267/initializes/init_autoflow
-PATH="~elenarojano/dev_gem/pets/bin":$PATH
-export PATH
-
-CODE_PATH=`pwd`
+export PATH="~elenarojano/dev_gem/pets/bin":$PATH
 export PATH=$CODE_PATH/aux_scripts:$PATH
 mode=$1 
 af_add_options=$2 #AutoFlow Additional options
 
-ln -s /mnt/home/users/bio_267_uma/josecordoba/software/cohortAnalyzer_wf/custom_opt ext_files/custom_opt
-custom_opt=$CODE_PATH'/ext_files/custom_opt'
 cohorts_path=$CODE_PATH"/tmp_files"
 
 #PATHS TO FILES:
 decipher_file="/mnt/home/users/bio_267_uma/jperkins/data/DECIPHER/decipher-cnvs-grch38-2022-05-15.txt"
-path_to_hpo='/mnt/home/users/bio_267_uma/elenarojano/dev_gem/pets/external_data/hp.obo'
-
-#SET INPUTS:
-
 cohorts='decipher'
-ln -s /mnt/home/users/bio_267_uma/josecordoba/software/cohortAnalyzer_wf/go-basic.obo ext_files/go-basic.obo
-path_to_GO=$CODE_PATH/ext_files/go-basic.obo  #define path to GO file in OBO format. Available at /mnt/home/users/bio_267_uma/josecordoba/software/cohortAnalyzer_wf
-path_to_annotations='/mnt/home/users/pab_001_uma/pedro/references' #define path to genome_version folder (must be configured in custom_opt file) that includes an annotation.gtf file
 
 #PROCEDURE:
 #1. Select DECIPHER patients
@@ -44,10 +34,6 @@ if [ "$mode" == "1" ]; then
 elif [ "$mode" == "2" ]; then
     AF_VARS=`echo -e "
         \\$all_cohorts=$cohorts,
-        \\$custom_options=$custom_opt,
-        \\$HPO=$path_to_hpo,
-        \\$annotation_path=$path_to_annotations,
-        \\$cohorts_path=$cohorts_path,
-        \\$GO=$path_to_GO" | tr -d [:space:]`
-    AutoFlow -e -w $CODE_PATH/templates/neuroanalysis_template.af -V $AF_VARS -o $CODE_PATH/results -c 2 -m 5gb -t '03:00:00' $af_add_options
+        \\$cohorts_path=$cohorts_path" | tr -d [:space:]`
+    AutoFlow -e -w $CODE_PATH/templates/neuroanalysis_template.af -V $AF_VARS -o $CODE_PATH/results -c 1 -m 5gb -t '03:00:00' $af_add_options
 fi
